1b473724c856774a28c94533bc485ab248394604
sjwarsame <167758567+sjwarsame@users.noreply.github.com>
2024-04-28 19:37:34 +0100

    Add files via upload

---
+++ week 1 assignment.py
@@ -0,0 +1,7 @@
+print("Welcome to the Weight Converter!")
+
+weight_kg = float(input("Please enter your weight in kilograms:  "))
+
+weight_lbs = weight_kg * 2.20462
+
+print(f"{weight_kg} kilograms is equivalent to {weight_lbs} pounds.")
